503d00f0e162a7dba5a741cf574924accb1c573f
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-12-30 16:06:04 +0100

    add tests

--- napari_brightness_contrast/_tests/test_dock_widget.py
+++ napari_brightness_contrast/_tests/test_dock_widget.py
@@ -1,18 +1,22 @@
 import napari_brightness_contrast
 import pytest
 
-# this is your plugin name declared in your napari.plugins entry point
-MY_PLUGIN_NAME = "napari-brightness-contrast"
-# the name of your widget(s)
-MY_WIDGET_NAMES = ["Example Q Widget", "example_magic_widget"]
 
-
-@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
-def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_manager):
-    napari_plugin_manager.register(napari_brightness_contrast, name=MY_PLUGIN_NAME)
+def test_something_with_viewer(make_napari_viewer):
     viewer = make_napari_viewer()
+
+    import numpy as np
+    image = np.random.random((100, 100))
+
+    viewer.add_image(image)
+
     num_dw = len(viewer.window._dock_widgets)
-    viewer.window.add_plugin_dock_widget(
-        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
-    )
+
+    from napari_brightness_contrast._dock_widget import BrightnessContrast
+    bc = BrightnessContrast(viewer)
+    viewer.window.add_dock_widget(bc)
     assert len(viewer.window._dock_widgets) == num_dw + 1
+
+    bc._set_absolutes()
+    bc._auto_percentiles()
+    bc._set_full_range()
